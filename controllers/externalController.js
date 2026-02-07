7fd4f7f0693c80df57a965315843aa97e2492e65
Bimbiz <bizinskiytimur@gmail.com>
2026-02-07 21:04:40 +0500

    frontend better

--- controllers/externalController.js
+++ controllers/externalController.js
@@ -1,21 +1,32 @@
-const axios = require('axios');
+const axios = require("axios");
 
 exports.getGamingNews = async (req, res) => {
-    try {
-        const search = req.query.search?.trim() || "gaming";
+  try {
+    const search =
+      req.query.search || '"video game" -casino -betting -gambling';
 
-        const response = await axios.get("https://api.apitube.io/v1/news/everything", {
-            params: {
-                query: search,
-                per_page: 10,
-            },
-            headers: {
-                Authorization: `Bearer ${process.env.APITUBE_API_KEY}`,
-            },
-        });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ message: "Failed to fetch gaming news", error: error.message });
-    }
+    const response = await axios.get(
+      "https://api.apitube.io/v1/news/everything",
+      {
+        params: {
+          query: search,
+          language: "en",
+          sort_by: "published_at",
+          per_page: 10,
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.APITUBE_API_KEY}`,
+        },
+      },
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to fetch gaming news",
+      error: error.message,
+    });
+  }
 };
-
